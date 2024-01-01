ab962dc6cf3665c56cb36b16bd4e3508e79e3af8
abdallah shamas <12033013@students.liu.edu.lb>
2024-01-01 17:15:22 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,22 @@
+// main.dart
+import 'package:flutter/material.dart';
+import 'register.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Recipe App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: RegisterPage(),
+    );
+  }
+}
